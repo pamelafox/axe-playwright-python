ca1046c69d14953f34ef0c432e95f718da178cb6
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2022-08-24 10:39:48 +0300

    feat: add selenium Axe class

---
+++ axe_core_python/selenium.py
@@ -0,0 +1,22 @@
+from base import AxeBase
+
+
+class Axe(AxeBase):
+    def run(
+        self,
+        webdriver,
+        context: str | list | dict | None = None,
+        options: dict | None = None,
+    ) -> dict:
+        # Recursively inject `Axe` into all iframes and the top level document
+        webdriver.execute_script(self.axe_script)
+
+        # Run `Axe` against the current page
+        args_str = self._format_script_args(context, options)
+        command_template = (
+            "var seleniumCallback = arguments[arguments.length - 1];"
+            "axe.run(%s).then(results => seleniumCallback(results))"
+        )
+        command = command_template % args_str
+        response = webdriver.execute_async_script(command)
+        return response
